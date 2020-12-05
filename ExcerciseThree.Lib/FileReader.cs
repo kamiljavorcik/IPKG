2f3520870f02b87ba24ce2e55a6dadd2ce5f3f0a
Kamil Javorčík <ext.kamil.javorcik@tieto.com>
2020-12-05 02:24:53 +0100

    Excercise Three Version 1

---
+++ ExcerciseThree.Lib/FileReader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcerciseThree.Lib
+{
+    public class FileReader : IFileReader
+    {
+        public string ReadText(string path)
+        {
+            if (System.IO.Path.GetExtension(path).ToLower() != "txt") throw new Exception("Wrong file type.");
+            return System.IO.File.ReadAllText(path);
+        }
+    }
+}
